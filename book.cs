d72732b4f1a4bd5fb1b28a6000ddac6b6e16f842
Hasan Akgün <akgunhasan993@gmail.com>
2026-09-18 17:21:44 +0300

    Add book model class

---
+++ book.cs
@@ -0,0 +1,9 @@
+namespace MyNamespace;
+
+public class Book
+{
+    public string Name { get; set; }
+    public string Writer  { get; set; }
+    public string Category  { get; set; }
+    public int Stock { get; set; }
+}
